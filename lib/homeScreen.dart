5e495c2b2fa7f87b94324c938645370fea36a8cf
ipobeatz <ipobeats7@gmail.com>
2024-05-21 04:14:03 +0300

    home screen design has been designed

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:word_wise/loginScreen.dart';
 import 'package:word_wise/splashScreen.dart';
 
@@ -27,28 +26,132 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 1; // Default olarak Home Page seçili olacak
+  double _leftCardOffset = 0;
+  double _rightCardOffset = 0;
+  double _mainCardOffsetX = 0;
+  double _mainCardOffsetY = 0;
+  String _mainCardText = "Main Card";
+  String _leftCardText = "Left";
+  String _rightCardText = "Right";
 
-  static List<Widget> _pages(BuildContext context) => <Widget>[
+  List<Widget> _pages(BuildContext context) => <Widget>[
     Center(child: Text('Saved Page')),
-    Center(
-      child: Container(
-        width: MediaQuery.of(context).size.width * 0.7,
-        height: MediaQuery.of(context).size.height * 0.7,
-        child: Card(
-          color: Colors.white,
-          elevation: 4,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15),
-          ),
-          child: Center(
-            child: Text(
-              'Welcome to Word Wise',
-              style: GoogleFonts.lato(fontSize: 24),
-              textAlign: TextAlign.center,
+    Stack(
+      children: [
+        AnimatedPositioned(
+          duration: Duration(milliseconds: 300),
+          top: MediaQuery.of(context).size.height * 0.15 + _mainCardOffsetY,
+          left: MediaQuery.of(context).size.width * 0.15 + _mainCardOffsetX,
+          right: MediaQuery.of(context).size.width * 0.15 - _mainCardOffsetX,
+          bottom: MediaQuery.of(context).size.height * 0.15 - _mainCardOffsetY,
+          child: GestureDetector(
+            onPanUpdate: (details) {
+              setState(() {
+                _mainCardOffsetX += details.delta.dx;
+                _mainCardOffsetY += details.delta.dy;
+              });
+            },
+            onPanEnd: (details) {
+              if (_mainCardOffsetX > 100) {
+                // Sağ tarafa kaydırıldı
+                setState(() {
+                  _mainCardOffsetX = MediaQuery.of(context).size.width;
+                  String temp = _mainCardText;
+                  _mainCardText = _rightCardText;
+                  _rightCardText = temp;
+                  _mainCardOffsetX = 0;
+                  _mainCardOffsetY = 0;
+                });
+              } else if (_mainCardOffsetX < -100) {
+                // Sol tarafa kaydırıldı
+                setState(() {
+                  _mainCardOffsetX = -MediaQuery.of(context).size.width;
+                  String temp = _mainCardText;
+                  _mainCardText = _leftCardText;
+                  _leftCardText = temp;
+                  _mainCardOffsetX = 0;
+                  _mainCardOffsetY = 0;
+                });
+              } else {
+                setState(() {
+                  _mainCardOffsetX = 0;
+                  _mainCardOffsetY = 0;
+                });
+              }
+            },
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.7,
+              height: MediaQuery.of(context).size.height * 0.7,
+              child: Card(
+                color: Colors.black38,
+                elevation: 4,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Center(
+                  child: Text(
+                    _mainCardText,
+                    style: GoogleFonts.lato(fontSize: 24),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
             ),
           ),
         ),
-      ),
+        Positioned(
+          top: MediaQuery.of(context).size.height * 0.10,
+          left: 0,
+          bottom: MediaQuery.of(context).size.height * 0.10, // Bu, sol card'ın tam yüksekliği kapsamasını sağlar
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.1,
+            height: MediaQuery.of(context).size.height * 0.6, // Bu, card'ın tam yüksekliği kapsamasını sağlar
+            child: Card(
+              color: Colors.red,
+              elevation: 8,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(35),
+                  bottomRight: Radius.circular(35),
+                ),
+              ),
+              child: Center(
+                child: Text(
+                  _leftCardText,
+                  style: GoogleFonts.lato(fontSize: 12),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          top: MediaQuery.of(context).size.height * 0.10,
+          right: 0,
+          bottom: MediaQuery.of(context).size.height * 0.10, // Bu, sağ card'ın tam yüksekliği kapsamasını sağlar
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.1,
+            height: MediaQuery.of(context).size.height * 0.6, // Bu, card'ın tam yüksekliği kapsamasını sağlar
+            child: Card(
+              color: Colors.blue,
+              elevation: 8,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(35),
+                  bottomLeft: Radius.circular(35),
+                ),
+              ),
+              child: Center(
+                child: Text(
+                  _rightCardText,
+                  style: GoogleFonts.lato(fontSize: 12),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
     ),
     Center(child: Text('Profile Page')),
   ];
